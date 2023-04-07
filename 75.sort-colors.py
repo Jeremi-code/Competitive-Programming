901d44e110004ee234d98a76e2745689938cbe26
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-04-07 05:49:10 -0700

    sorting colors based on their integer value

---
+++ 75.sort-colors.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode id=75 lang=python3
+#
+# [75] Sort Colors
+#
+
+# @lc code=start
+class Solution:
+    from typing import List
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+                """
+        for i in range(1,len(nums)):
+            for j in range(i,0,-1):
+                if nums[j]<nums[j-1]:
+                    nums[j],nums[j-1]=nums[j-1],nums[j]
+                else:
+                    break
+        print(nums)
+
+
+# @lc code=end
+
