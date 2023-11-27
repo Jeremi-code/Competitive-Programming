1db5d6d1c7fb66a89908d1ad2cc9e03a177e3201
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-11-27 04:45:47 -0800

    number of good pairs

---
+++ 1512.number-of-good-pairs.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode id=1512 lang=python3
+#
+# [1512] Number of Good Pairs
+#
+
+# @lc code=start
+from typing import List
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        count = 0
+        for i in range (0,len(nums)):
+            for j in range (i+1,len(nums)):
+                if (nums[i] == nums[j]) :
+                    count +=1
+        return count
+        
+# @lc code=end
+
