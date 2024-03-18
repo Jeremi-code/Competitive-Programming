120692b82a2a5d4258457cfa10cc539a29ada68c
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-03-18 12:21:20 -0700

    fix : missed edge case

--- A_Spell_Check.py
+++ A_Spell_Check.py
@@ -2,17 +2,21 @@ n = int(input(''))
 while n :
     m = int(input(''))
     t = "Tumri"
-    s = list(input(""))
+    dict = {"T" : 0,"u" : 0, "m" : 0, "r" : 0,"i" : 0}
     count = 0
+    s = input("").strip()
     for i in range(m):
-        if s[i] in t :
-            count +=1
+        if s[i] in t and m == 5:
+            dict[s[i]] +=1
+            count += 1
         else :
             break
-    if count == 5 :
+    all_one = all(value == 1 for value in dict.values())
+    if count == 5 and all_one == True:
         print('YES')
     else :
         print('NO')
+
     n-=1
             
 
